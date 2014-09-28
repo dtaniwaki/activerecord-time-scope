44d28e7f82011b56675b7bdef8597c530b1f06be
dtaniwaki <daisuketaniwaki@gmail.com>
2014-09-28 23:52:24 +0900

    Reorganize time proxy spec

--- spec/time_scope/time_range_spec.rb
+++ spec/time_scope/time_range_spec.rb
@@ -1,21 +1,41 @@
 require 'spec_helper'
 
 RSpec.describe ActiveRecord::TimeScope::TimeRangeProxy do
-  klass = create_tmp_model foo: :datetime, bar: :datetime do
-    create_time_range_scope :wow, :foo, :bar
+  let(:klass) do
+    create_tmp_model foo: :datetime, bar: :datetime do
+      create_time_range_scope :wow, :foo, :bar
+    end
   end
 
   describe "::wow_before" do
     subject { klass.__send__("wow_before", time) }
     let(:time) { DateTime.now }
-    context "foo & bar < time" do
+    # .=====.
+    # ......!
+    context "foo < bar < time" do
       let!(:model) { klass.create! foo: time - 2.days, bar: time - 1.days }
       it { is_expected.to eq [model] }
     end
-    context "time < foo & bar" do
+    # .=====.
+    # .....!.
+    context "foo < bar = time" do
+      let!(:model) { klass.create! foo: time - 2.days, bar: time - 1.days }
+      it { is_expected.to eq [model] }
+    end
+    # .=====.
+    # !......
+    context "time < foo < bar" do
       let!(:model) { klass.create! foo: time + 1.days, bar: time + 2.days }
       it { is_expected.to eq [] }
     end
+    # .=====.
+    # .!.....
+    context "time = foo < bar" do
+      let!(:model) { klass.create! foo: time + 1.days, bar: time + 2.days }
+      it { is_expected.to eq [] }
+    end
+    # .=====.
+    # ...!...
     context "foo < time < bar" do
       let!(:model) { klass.create! foo: time - 1.days, bar: time + 1.days }
       it { is_expected.to eq [] }
@@ -24,14 +44,32 @@ RSpec.describe ActiveRecord::TimeScope::TimeRangeProxy do
   describe "::wow_after" do
     subject { klass.__send__("wow_after", time) }
     let(:time) { DateTime.now }
-    context "foo & bar < time" do
+    # .=====.
+    # ......!
+    context "foo < bar < time" do
       let!(:model) { klass.create! foo: time - 2.days, bar: time - 1.days }
       it { is_expected.to eq [] }
     end
-    context "time < foo & bar" do
+    # .=====.
+    # .....!.
+    context "foo < bar = time" do
+      let!(:model) { klass.create! foo: time - 2.days, bar: time - 1.days }
+      it { is_expected.to eq [] }
+    end
+    # .=====.
+    # !......
+    context "time < foo < bar" do
       let!(:model) { klass.create! foo: time + 1.days, bar: time + 2.days }
       it { is_expected.to eq [model] }
     end
+    # .=====.
+    # .!.....
+    context "time = foo < bar" do
+      let!(:model) { klass.create! foo: time + 1.days, bar: time + 2.days }
+      it { is_expected.to eq [model] }
+    end
+    # .=====.
+    # ...!...
     context "foo < time < bar" do
       let!(:model) { klass.create! foo: time - 1.days, bar: time + 1.days }
       it { is_expected.to eq [] }
@@ -40,28 +78,70 @@ RSpec.describe ActiveRecord::TimeScope::TimeRangeProxy do
   describe "::wow_within" do
     subject { klass.__send__("wow_within", time - 2.days, time + 2.days) }
     let(:time) { DateTime.now }
-    context "time - 2.days < foo & bar < time + 2.days" do
+    # .=====.
+    # .!....!
+    context "time1 = foo < bar < time2" do
+      let!(:model) { klass.create! foo: time - 2.days, bar: time + 1.days }
+      it { is_expected.to eq [] }
+    end
+    # .=====.
+    # !.....!
+    context "time1 < foo < bar < time2" do
       let!(:model) { klass.create! foo: time - 1.days, bar: time + 1.days }
       it { is_expected.to eq [model] }
     end
-    context "time - 2.days < foo < time + 2.days < bar" do
+    # .=====.
+    # !.....!.
+    context "time1 < foo < bar = time2" do
+      let!(:model) { klass.create! foo: time - 1.days, bar: time + 2.days }
+      it { is_expected.to eq [] }
+    end
+    # .=====.
+    # !...!..
+    context "time1 < foo < time2 < bar" do
       let!(:model) { klass.create! foo: time + 1.days, bar: time + 3.days }
       it { is_expected.to eq [] }
     end
-    context "foo < time - 2.days < bar < time + 2.days" do
+    # .=====.
+    # ...!..!
+    context "foo < time1 < bar < time2" do
       let!(:model) { klass.create! foo: time - 3.days, bar: time - 1.days }
       it { is_expected.to eq [] }
     end
-    context "foo < time - 2.days < time + 2.days < bar" do
-      let!(:model) { klass.create! foo: time - 3.days, bar: time + 3.days }
-      it { is_expected.to eq [] }
-    end
-    context "time - 2.days < time + 2.days < foo & bar" do
+    # ..=====
+    # !!.....
+    context "time1 < time2 < foo < bar" do
       let!(:model) { klass.create! foo: time + 3.days, bar: time + 4.days }
       it { is_expected.to eq [] }
     end
-    context "foo & bar < time - 2.days < time + 2.days" do
+    # ..=====
+    # !.!....
+    context "time1 < time2 = foo < bar" do
+      let!(:model) { klass.create! foo: time + 2.days, bar: time + 4.days }
+      it { is_expected.to eq [] }
+    end
+    # =====..
+    # .....!!
+    context "foo < bar < time1 < time2" do
       let!(:model) { klass.create! foo: time - 4.days, bar: time - 3.days }
+      it { is_expected.to eq [] }
+    end
+    # =====..
+    # ....!.!
+    context "foo < bar = time1 < time2" do
+      let!(:model) { klass.create! foo: time - 4.days, bar: time - 2.days }
+      it { is_expected.to eq [] }
+    end
+    # .=====.
+    # .!...!.
+    context "foo = time1 < time2 = bar" do
+      let!(:model) { klass.create! foo: time - 2.days, bar: time + 2.days }
+      it { is_expected.to eq [] }
+    end
+    # .=====.
+    # ..!.!..
+    context "foo < time1 < time2 < bar" do
+      let!(:model) { klass.create! foo: time - 3.days, bar: time + 3.days }
       it { is_expected.to eq [] }
     end
   end
--- spec/time_scope/time_spec.rb
+++ spec/time_scope/time_spec.rb
@@ -1,46 +1,85 @@
 require 'spec_helper'
 
 RSpec.describe ActiveRecord::TimeScope::TimeProxy do
-  klass = create_tmp_model foo: :datetime, bar_on: :date do
-    create_time_scope :foo, :foo
-    create_time_scope :bar, :bar
+  let(:time) { DateTime.now }
+  let(:klass) do
+    create_tmp_model foo: :datetime, bar_on: :date do
+      create_time_scope :foo, :foo
+      create_time_scope :bar, :bar
+    end
   end
 
   describe "::foo_before" do
     subject { klass.foo_before(time) }
-    let(:time) { DateTime.now }
+    # .=...
+    # ..!..
     context "foo < time" do
       let!(:model) { klass.create! foo: time + 1.days }
       it { is_expected.to eq [] }
     end
+    # ...=.
+    # ..!..
     context "time < foo" do
       let!(:model) { klass.create! foo: time - 1.days }
       it { is_expected.to eq [model] }
+    end
+    # ..=..
+    # ..!..
+    context "time = foo" do
+      let!(:model) { klass.create! foo: time }
+      it { is_expected.to eq [] }
     end
   end
   describe "::foo_after" do
     subject { klass.foo_after(time) }
-    let(:time) { DateTime.now }
+    # .=...
+    # ..!..
     context "foo < time" do
       let!(:model) { klass.create! foo: time + 1.days }
       it { is_expected.to eq [model] }
     end
+    # ...=.
+    # ..!..
     context "time < foo" do
       let!(:model) { klass.create! foo: time - 1.days }
+      it { is_expected.to eq [] }
+    end
+    # ..=..
+    # ..!..
+    context "time = foo" do
+      let!(:model) { klass.create! foo: time }
       it { is_expected.to eq [] }
     end
   end
   describe "::foo_within" do
     subject { klass.foo_within(time - 2.days, time + 2.days) }
     let(:time) { DateTime.now }
-    context "foo < -2 days < +2 days" do
+    # .=...
+    # ..!!.
+    context "foo < time1 < time2" do
       let!(:model) { klass.create! foo: time - 3.days }
       it { is_expected.to eq [] }
     end
-    context "-2 days < foo < +2 days" do
+    # .=...
+    # .!!..
+    context "foo = time1 < time2" do
+      let!(:model) { klass.create! foo: time - 3.days }
+      it { is_expected.to eq [] }
+    end
+    # ..=..
+    # .!.!.
+    context "time1 < foo < time2" do
       let!(:model) { klass.create! foo: time }
       it { is_expected.to eq [model] }
     end
+    # ...=.
+    # ..!!.
+    context "time1 < time2 = foo" do
+      let!(:model) { klass.create! foo: time + 3.days }
+      it { is_expected.to eq [] }
+    end
+    # ...=.
+    # .!!..
     context "-2 days < +2 days < foo" do
       let!(:model) { klass.create! foo: time + 3.days }
       it { is_expected.to eq [] }
